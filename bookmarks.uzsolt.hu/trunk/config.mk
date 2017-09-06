fb5efdb18d1eef0100ef2c92f448b5ced48f0fcc
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2017-09-06 09:46:17 +0000

    REQ-ek rendbetétele
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@9 a3c0a338-e892-e711-a987-005056a07bed

--- bookmarks.uzsolt.hu/trunk/config.mk
+++ bookmarks.uzsolt.hu/trunk/config.mk
@@ -5,7 +5,7 @@ INCLUDE_DIR=include/
 VIRT_DIR=virtual/
 
 TARGETS=index.html
-index.html_REQ=include/header.m4 scripts/generate_main_list.sh scripts/get_title.sh
+index.html_REQ=include/header.m4 scripts/generate_main_list.sh scripts/get_title.sh data/title.psv
 TARGETS_MANUAL=bookmarks.css
 
 VIRTUALS=	links
@@ -14,12 +14,13 @@ VIRTUALTEMPLATE_links=	links
 VIRTUALDIR_links=
 VIRTUALOUT_links!=sed 's,$$,/index.html,' data/main.psv
 
-VIRTUALREQ_links=include/header.m4 scripts/get_title.sh scripts/list_links.sh gen/navbar.html
+VIRTUALREQ_links=include/header.m4 scripts/get_title.sh scripts/list_links.sh gen/navbar.html data/title.psv
 VIRTUALREQRULE_links=C,(.*)/index.html,data/\1.psv,
-torvenyek/index.html_REQ!=ls data/torvenyek_*.psv
-hivatalos/index.html_REQ!=ls data/hivatalos_*.psv
+.for f in bethlen hivatalos torvenyek
+${f}/index.html_REQ!=ls data/${f}_*.psv
+.endfor
 
-gen/navbar.html: data/main.psv scripts/generate_main_list.sh
+gen/navbar.html: data/main.psv scripts/generate_main_list.sh data/title.psv
 	@scripts/generate_main_list.sh > ${.TARGET}
 
 ${DEST_DIR}bookmarks.css: assets/bookmarks.css
