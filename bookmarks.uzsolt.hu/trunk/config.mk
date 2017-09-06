5f6f9bab8ee01217f85331a5cfe362db7e42245d
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2017-09-06 09:46:17 +0000

    hivatalos csoportokra bontva
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@8 a3c0a338-e892-e711-a987-005056a07bed

--- bookmarks.uzsolt.hu/trunk/config.mk
+++ bookmarks.uzsolt.hu/trunk/config.mk
@@ -16,7 +16,8 @@ VIRTUALOUT_links!=sed 's,$$,/index.html,' data/main.psv
 
 VIRTUALREQ_links=include/header.m4 scripts/get_title.sh scripts/list_links.sh gen/navbar.html
 VIRTUALREQRULE_links=C,(.*)/index.html,data/\1.psv,
-torvenyek/index.html_REQ+=data/torvenyek_oktatas.psv
+torvenyek/index.html_REQ!=ls data/torvenyek_*.psv
+hivatalos/index.html_REQ!=ls data/hivatalos_*.psv
 
 gen/navbar.html: data/main.psv scripts/generate_main_list.sh
 	@scripts/generate_main_list.sh > ${.TARGET}
