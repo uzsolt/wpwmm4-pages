b638325a2ac84c76fc8b359650511d6bcf3667c0
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2018-04-01 10:36:20 +0000

    accesskey hozzáadása
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@32 a3c0a338-e892-e711-a987-005056a07bed

--- bookmarks.uzsolt.hu/trunk/scripts/generate_main_list.sh
+++ bookmarks.uzsolt.hu/trunk/scripts/generate_main_list.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+ACCESSKEYS="0123456789abcdefghijklmnopqrstuvwxyz"
+
+counter=1
 for f in `cat data/main.psv`; do
   TITLE=`grep "${f}|" data/title.psv | awk -F '|' '{print $2}'`
-  printf '<a href="%s">%s</a>\n' "/${f}" "${TITLE}"
+  ak=$(echo ${ACCESSKEYS} | cut -c ${counter})
+  printf '<a href="%s" accesskey="%s"><span class="accesskey">%s</span>%s</a>\n' "/${f}" "${ak}" "${ak}" "${TITLE}"
+  counter=$((counter+1))
 done
--- bookmarks.uzsolt.hu/trunk/scripts/list_links.sh
+++ bookmarks.uzsolt.hu/trunk/scripts/list_links.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 IFS="|"
+ACCESSKEYS="0123456789abcdefghijklmnopqrstuvwxyz"
+
 FILES=`find data/ -name ${1}_\*.psv | tr '\n' '|'`
 
-printf '<div id="maingroup">\n<h1>_ACTIVEMENU</h1>\n'
+printf '<h1>_ACTIVEMENU</h1>\n<div id="maingrouptable">\n<div id="maingroup">\n'
+counter=`cat data/main.psv | wc -l | sed "s@^ *@@"`
+counter=$((counter+1))
 for f in data/${1}.psv ${FILES}; do
   FWP=`echo ${f} | sed 's,data/\(.*\)\.psv,\1,'`
   printf '  <div class="group">\n'
   printf '    <h2>%s</h2>\n' "`grep \"${FWP}|\" data/title.psv | awk -F '|' '{print $2}'`"
   cat ${f} | while read name url; do
-    printf '    <a href="%s"><div class="grouphref">%s</div></a>\n' "${url}" "${name}"
+    ak=`echo ${ACCESSKEYS} | cut -c ${counter}`
+    printf '    <a href="%s" accesskey="%s"><div class="grouphref"><span class="accesskey">%s</span><span class="href">%s</span></div></a>\n' "${url}" "${ak}" "${ak}" "${name}"
+    counter=$((counter+1))
   done
+  lf=`cat ${f} | wc -l`
+  counter=$((counter+lf))
   printf '  </div>\n'
 done
-printf '</div>'
+printf '</div></div>'
