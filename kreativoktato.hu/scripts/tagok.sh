10722e72f63afb4afe71f508514aa3c9f90be147
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2019-10-02 16:01:44 +0000

    EKO elé 'az' A végzettséget m4-idézőjelbe rakjuk, hogy a vessző utáni szóközt ne törölje.
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@186 a3c0a338-e892-e711-a987-005056a07bed

--- kreativoktato.hu/scripts/tagok.sh
+++ kreativoktato.hu/scripts/tagok.sh
@@ -17,8 +17,8 @@ print_nevjegyek() {
     printf "  <a href='%s'><div class='tagnevjegy'>\n\
       <img src='pict/%s' alt='kép'>\n\
       <span class='tagnevjegy_nev'>%s</span><br>\n\
-      <span class='tagnevjegy_vegz'>%s</span><br>\n\
-      <span class='tagnevjegy_eko'>%s</span><br>\n\
+      <span class='tagnevjegy_vegz'>\`%s\'</span><br>\n\
+      <span class='tagnevjegy_eko'>az %s</span><br>\n\
     </div></a>\n" \
       "#${MT_PREFIX}${file}" \
       "${file}.jpg" \
@@ -36,7 +36,7 @@ print_bemutatkozasok() {
     printf "    <div class='tagimg'><img src='pict/%s' alt='%s'/></div>\n" "${file}.jpg" "${nev} fényképe"
     printf "    <div class='tagleiras'>\n"
     printf "    <span class='tagnev'>%s (%s)</span><br>\n" "${nev}" "`head -n1 ${tagfile}`"
-    printf "    <p>%s</p>\n" "`sed -n '2p' ${tagfile}`"
+    printf "    <p><em>%s</em></p>\n" "az `sed -n '2p' ${tagfile}`"
     sed '1,2d ; s,^,    ,' ${tagfile}
     printf "    </div>\n"
     printf "  %s\n" "${hrefvissza}"
