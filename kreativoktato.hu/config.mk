1cbb639a0c4d8c78710b2c794639ee684af1895e
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2019-10-28 16:44:45 +0000

    A menüben kicsi szélességkor egy erősebb háttér jelenik meg, és a fő kép nem.
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@233 a3c0a338-e892-e711-a987-005056a07bed

--- kreativoktato.hu/config.mk
+++ kreativoktato.hu/config.mk
@@ -67,8 +67,8 @@ ${DEST_DIR}${PICTDIR}/logo-tr.png:	static/logo/logo-small-200px-01.png
 	@convert -alpha on -channel a -evaluate set 5% $> $@
 
 ${DEST_DIR}${PICTDIR}/logo-tr0.png:	static/logo/logo-small-200px-01.png
-	@convert -alpha on -channel a -evaluate set 10% $> tmp.png
-	@convert -resize 128 tmp.png $@
+	@convert -alpha on -channel a -evaluate set 15% $> tmp.png
+	@convert -resize 200 tmp.png $@
 	@rm tmp.png
 
 ${DEST_DIR}${PICTDIR}/logo-menu.png:	static/logo/logo-small-200px-01.png
