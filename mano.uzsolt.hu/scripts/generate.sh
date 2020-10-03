9a6d2467dbc71071b8f6da88be434489b9286149
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2020-10-03 20:18:18 +0000

    format_url - escaping ampersand
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@312 a3c0a338-e892-e711-a987-005056a07bed

--- mano.uzsolt.hu/scripts/generate.sh
+++ mano.uzsolt.hu/scripts/generate.sh
@@ -4,11 +4,15 @@ get_field() {
   echo "${1}" | awk -F '|' -v param=$2 '{print $param}'
 }
 
+format_url() {
+  echo "${*}" | sed 's,\&,%26,g'
+}
+
 # title
 # icon
 # url
 print_div() {
-  printf '\t<a href="%s"><div class="%s">\n' "$3" "container"
+  printf '\t<a href="%s"><div class="%s">\n' "$(format_url $3)" "container"
   printf '\t\t<div class="descr">%s</div>\n' "$1"
   printf '\t\t<div class="%s">\n' "pictdiv"
   printf '\t\t<span class="helper"></span><img src="icons/%s" alt="%s" class="%s"/>\n' "$2" "$1" "pict"
