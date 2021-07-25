23f57a4ce2377719219e88ce8c483a1660bbc4d8
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2021-07-25 10:19:03 +0000

    eko.css függőségeit automatikusan generáljuk
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@333 a3c0a338-e892-e711-a987-005056a07bed

--- kreativoktato.hu/trunk/mk/css.mk
+++ kreativoktato.hu/trunk/mk/css.mk
@@ -1,5 +1,6 @@
 TARGETS_MANUAL+=	www_static/css/eko.css
-${ASSETS_DEST_DIR}css/eko.css:	static/eko.scss static/pdf.scss static/responsive.scss static/table.scss
+EKO.CSS_DEPS!=	sed -n '/^@import/ s,.*"\([^"]*\)";,static/\1,p' static/eko.scss
+${ASSETS_DEST_DIR}css/eko.css:	static/eko.scss ${EKO.CSS_DEPS}
 	@mkdir -p ${ASSETS_DEST_DIR}css
 	sassc -t compressed ${>:[1]} $@
 
