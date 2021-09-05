e297698369818c9f23e8b8869b731e55c55b547c
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2021-09-05 03:37:34 +0000

    fasf.scss szétszedése külön fájlokra
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@393 a3c0a338-e892-e711-a987-005056a07bed

--- kreativoktato.hu/trunk/mk/css.mk
+++ kreativoktato.hu/trunk/mk/css.mk
@@ -5,6 +5,7 @@ ${ASSETS_DEST_DIR}css/eko.css:	static/eko.scss ${EKO.CSS_DEPS}
 	sassc -t compressed ${>:[1]} $@
 
 TARGETS_MANUAL+=	www_static/css/fasf.css
+FASF.CSS_DEPS!=	sed -n '/^@import/ s,.*"\([^"]*\)";,static/\1,p' static/fasf.scss
 ${ASSETS_DEST_DIR}css/fasf.css:	static/fasf.scss
 	@mkdir -p ${ASSETS_DEST_DIR}css
 	sassc -t compressed ${>:[1]} $@
