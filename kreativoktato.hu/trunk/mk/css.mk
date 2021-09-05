c599be44667ee5674ed9013ed7870084159ab507
zsolt <zsolt@a3c0a338-e892-e711-a987-005056a07bed>
2021-09-05 03:41:01 +0000

    fasf.css függőségek hozzáadása
    git-svn-id: file:///usr/local/subversion/wpwmm4-pages@394 a3c0a338-e892-e711-a987-005056a07bed

--- kreativoktato.hu/trunk/mk/css.mk
+++ kreativoktato.hu/trunk/mk/css.mk
@@ -6,6 +6,6 @@ ${ASSETS_DEST_DIR}css/eko.css:	static/eko.scss ${EKO.CSS_DEPS}
 
 TARGETS_MANUAL+=	www_static/css/fasf.css
 FASF.CSS_DEPS!=	sed -n '/^@import/ s,.*"\([^"]*\)";,static/\1,p' static/fasf.scss
-${ASSETS_DEST_DIR}css/fasf.css:	static/fasf.scss
+${ASSETS_DEST_DIR}css/fasf.css:	static/fasf.scss ${FASF.CSS_DEPS}
 	@mkdir -p ${ASSETS_DEST_DIR}css
 	sassc -t compressed ${>:[1]} $@
